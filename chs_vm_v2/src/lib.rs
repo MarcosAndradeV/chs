1bd7678d45642833f7a1117d0c147da18def9a51
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-08-24 21:56:47 -0300

    [chs_vm_v2] Using Memory implementation for the stack

--- chs_vm_v2/src/lib.rs
+++ chs_vm_v2/src/lib.rs
@@ -1,5 +1,9 @@
 pub mod instructions;
+use core::fmt;
+use std::marker::PhantomData;
+
 use instructions::{Bytecode, Instr};
+use memory::Memory;
 
 pub fn jump(addr: usize, rel: isize) -> usize {
     (addr as isize + rel) as usize
@@ -8,10 +12,55 @@ pub fn jump_to(addr: usize, other: usize) -> isize {
     other as isize - addr as isize
 }
 
-type Value = i32;
+type Value = u64;
+
+#[derive(Debug)]
+struct VMStack<T: Sized> {
+    marker: PhantomData<T>,
+    data: Memory,
+    top: usize,
+}
+
+impl fmt::Display for VMStack<Value> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut buf = String::from("[");
+        let mut i = self.data.size() - size_of::<Value>();
+        while i >= self.top {
+            buf.push_str(&format!(" {} ", self.data.read::<Value>(i)));
+            i -= size_of::<Value>()
+        }
+        write!(f, "{}]", buf)
+    }
+}
+
+impl VMStack<Value> {
+    pub fn new(size: usize) -> Self {
+        let data = Memory::new(size);
+        let top = size; // - size_of::<Value>();
+        Self {
+            data,
+            top,
+            marker: PhantomData::default(),
+        }
+    }
+    pub fn push(&mut self, value: Value) {
+        self.top -= size_of::<Value>();
+        self.data.write(self.top, value);
+        self.data.set_write_pos(self.top);
+    }
+    pub fn pop(&mut self) -> Value {
+        let index = self.top;
+        self.top += size_of::<Value>();
+        self.data.read(index)
+    }
+    pub fn len(&self) -> usize {
+        self.data.size() / size_of::<Value>()
+    }
+}
 
 pub fn vm_run(program: Bytecode) {
-    let mut stack: Vec<Value> = Vec::with_capacity(1024);
+    // let mut stack: Vec<Value> = Vec::with_capacity(1024);
+    let mut stack = VMStack::<Value>::new(1024);
     // let mut tstack: Vec<Value> = Vec::with_capacity(1024);
     // let mut rstack: Vec<usize> = Vec::with_capacity(1024);
     let mut ip = program.entry;
@@ -25,23 +74,23 @@ pub fn vm_run(program: Bytecode) {
             Instr::Halt => {
                 next_addr = program.program.len();
             }
-            Instr::PushI32(v) => stack.push(v),
+            Instr::PushI32(v) => stack.push(v as u64),
             Instr::Drop => {
-                stack.pop().unwrap();
+                stack.pop();
             }
             Instr::Dup => {
-                let a = stack.pop().unwrap();
+                let a = stack.pop();
                 stack.push(a);
                 stack.push(a);
             }
             Instr::Debug => {
-                println!("Debug:\nData Stack: {:?}", stack);
+                println!("Debug:\nData Stack: {}", stack);
             }
             Instr::Jmp(rel_addr) => {
                 next_addr = jump(ip, rel_addr);
             }
             Instr::JmpIf(rel_addr) => {
-                let test = stack.pop().unwrap();
+                let test = stack.pop();
                 if test == 0 {
                     next_addr = jump(ip, rel_addr);
                 }
@@ -50,29 +99,30 @@ pub fn vm_run(program: Bytecode) {
                 next_addr = abs_addr;
             }
             Instr::AJmpIf(abs_addr) => {
-                let test = stack.pop().unwrap();
+                let test = stack.pop();
                 if test == 0 {
                     next_addr = abs_addr;
                 }
             }
             Instr::PlusI => {
-                let a = stack.pop().unwrap();
-                let b = stack.pop().unwrap();
+                let a = stack.pop();
+                let b = stack.pop();
                 stack.push(a + b);
             }
             Instr::EqI => {
-                let a = stack.pop().unwrap();
-                let b = stack.pop().unwrap();
-                stack.push((a == b) as i32);
+                let a = stack.pop();
+                let b = stack.pop();
+                stack.push((a == b) as u64);
             }
             Instr::NEqI => {
-                let a = stack.pop().unwrap();
-                let b = stack.pop().unwrap();
-                stack.push((a != b) as i32);
+                let a = stack.pop();
+                let b = stack.pop();
+                stack.push((a != b) as u64);
             }
             Instr::Bind(rel) => {
-                assert!(stack.len() >= rel as usize);
-                stack.push(stack[stack.len() - rel as usize]);
+                let rel = rel as usize * size_of::<Value>();
+                assert!(stack.len() >= rel && rel <= stack.top);
+                stack.push(stack.data.read((stack.top + rel) - size_of::<Value>()));
             }
         }
         ip = next_addr
---
+++ memory/src/lib.rs
@@ -0,0 +1,137 @@
+use std::{
+    alloc::{alloc, alloc_zeroed, dealloc, Layout},
+    io::Write,
+};
+
+#[derive(Debug)]
+pub struct Memory {
+    inner: *mut u8, // Vec<u8>,
+    layout: Layout, // TODO: Remove this
+    write_pos: usize,
+    size: usize,
+}
+
+/// Types must implement this to work with `Memory`
+///
+/// ### !! Warning !!
+///
+/// Non-primitive types who implements this trait
+/// can cause the program to crash, because of malformation of the type.
+///
+/// In implemetation below the program will crash because of the is no way tho construct `Foo` from `1u8`.
+/// ```
+/// #[repr(u8)]
+/// enum Foo {
+///     Bar = 0u8,
+/// }
+/// unsafe impl MemoryAllowed for Foo {}
+/// let mut mem = Memory::new(16);
+/// mem.write(0, 1u8);
+/// mem.read::<Foo>(0);
+/// ```
+///
+pub unsafe trait MemoryAllowed {}
+unsafe impl MemoryAllowed for u8 {}
+unsafe impl MemoryAllowed for u16 {}
+unsafe impl MemoryAllowed for u32 {}
+unsafe impl MemoryAllowed for u64 {}
+unsafe impl MemoryAllowed for i8 {}
+unsafe impl MemoryAllowed for i16 {}
+unsafe impl MemoryAllowed for i32 {}
+unsafe impl MemoryAllowed for i64 {}
+
+const MEM_MIN: usize = 16;
+
+impl Drop for Memory {
+    fn drop(&mut self) {
+        unsafe { dealloc(self.inner, self.layout) };
+    }
+}
+
+impl Default for Memory {
+    fn default() -> Self {
+        Self::new(MEM_MIN)
+    }
+}
+
+impl Memory {
+    pub fn new(size: usize) -> Self {
+        let size = if size > MEM_MIN { size } else { MEM_MIN };
+        let layout = Layout::from_size_align(size, size_of::<u8>()).unwrap();
+        Self {
+            inner: unsafe { alloc_zeroed(layout) }, //vec![0; size],
+            layout,
+            write_pos: 0,
+            size,
+        }
+    }
+
+    pub fn realloc(&mut self, new_size: usize) {
+        assert!(new_size > self.size, "Cannot realloc less memory");
+        unsafe {
+            let new_layout = Layout::from_size_align(new_size, size_of::<u8>()).unwrap();
+            let new_inner = alloc(new_layout);
+            new_inner.copy_from(self.inner, self.size);
+            dealloc(self.inner, self.layout);
+            self.inner = new_inner;
+            self.layout = new_layout;
+            self.size = new_size;
+        }
+    }
+
+    pub fn write_push<T: Copy + MemoryAllowed>(&mut self, value: T) {
+        let size_of = size_of::<T>();
+        if self.write_pos + size_of >= self.size {
+            self.realloc(self.size + size_of);
+        }
+        self.write(self.write_pos, value);
+        self.write_pos += size_of;
+    }
+
+    pub fn read_push<T: Copy + MemoryAllowed>(&mut self) -> T {
+        let size_of = size_of::<T>();
+        assert!(self.write_pos + size_of <= self.size);
+        let res = self.read::<T>(self.write_pos);
+        self.write_pos += size_of;
+
+        res
+    }
+
+    pub fn read<T: Copy + MemoryAllowed>(&self, index: usize) -> T {
+        let size_of = size_of::<T>();
+        let byte_index = index;
+        assert!(byte_index + size_of <= self.size, "Index out of bounds");
+        unsafe { (self.inner.add(index) as *const T).read_unaligned() }
+    }
+
+    pub fn write<T: Copy + MemoryAllowed>(&mut self, index: usize, value: T) {
+        let size_of = size_of::<T>();
+        let byte_index = index;
+        assert!(byte_index + size_of <= self.size, "Index out of bounds");
+        unsafe {
+            (self.inner.add(index) as *mut T).write_unaligned(value);
+        }
+    }
+
+    pub fn into_bytes(self) -> Vec<u8> {
+        let mut buf: Vec<u8> = Vec::new();
+        unsafe {
+            if let Some(a) = std::ptr::slice_from_raw_parts(self.inner, self.size).as_ref() {
+                let _ = buf.write(a);
+            }
+        }
+        buf
+    }
+
+    pub fn set_write_pos(&mut self, pos: usize) {
+        self.write_pos = pos;
+    }
+
+    pub fn get_write_pos(&self) -> usize {
+        self.write_pos
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+}
